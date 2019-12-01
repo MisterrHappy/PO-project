0ceec58e07a4187ae074be401dde5a78a877ad09
MisterrHappy <andremarinho_1@hotmail.com>
2019-12-01 00:55:36 +0000

    ...

--- POFolder/src/m19/core/Notification.java
+++ POFolder/src/m19/core/Notification.java
@@ -1,1 +1,9 @@
 package m19.core;
+
+public class Notification {
+    private String _message;
+
+    Notification(String message) {
+        _message = message;
+    }
+}
--- POFolder/src/m19/core/Request.java
+++ POFolder/src/m19/core/Request.java
@@ -1,5 +1,15 @@
 package m19.core;
 
 public class Request {
+    private User _user;
+    private Work _work;
+    private int _deadline;
+    
+    Request(User user, Work work, int deadline) {
+        _user = user;
+        _work = work;
+        _deadline = deadline;
+    }
+
     
 }
